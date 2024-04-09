524fab4848cb1a340c0e2f1c40d18ca70aa68ed7
sandesh <sandeshchapagai15@gmail.com>
2024-04-09 16:36:28 +0545

    marker added

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -2,31 +2,42 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 class Home extends StatefulWidget {
-  const Home({super.key});
+  const Home({Key? key}) : super(key: key);
 
   @override
   State<Home> createState() => _HomeState();
 }
 
-class _HomeState extends State<Home>{
-  Completer<GoogleMapController> _controller = Completer();
-static final CameraPosition _kGoogle = const CameraPosition(
-    target:LatLng(28.237988,83.995590),zoom:10
+class _HomeState extends State<Home> {
+  List<Marker> _marker = [
+    Marker(
+      markerId: MarkerId('1'),
+      position: LatLng(28.237988, 83.995590),
+    )
+  ];
 
-);
+  static final CameraPosition _kGoogle = const CameraPosition(
+    target: LatLng(28.237988, 83.995590),
+    zoom: 10,
+  );
+
+  Completer<GoogleMapController> _controller = Completer();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:GoogleMap(
-initialCameraPosition: _kGoogle,
+      body: GoogleMap(
+        initialCameraPosition: _kGoogle,
+        markers: Set<Marker>.of(_marker),
         // mapType: MapType.hybrid,
         myLocationButtonEnabled: true,
         myLocationEnabled: true,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
-      )
+      ),
     );
   }
 }
