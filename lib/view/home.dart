e97b52ee1eecd0bce22b16d19dda93495a050afc
sandesh <sandeshchapagai15@gmail.com>
2024-04-12 20:32:55 +0545

    aaded the button which navigate to the setted location

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -11,11 +10,16 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<Marker> _marker = [
+  final List<Marker> _marker = [
     Marker(
-      markerId: MarkerId('1'),
-      position: LatLng(28.237988, 83.995590),
-    )
+        markerId: MarkerId('1'),
+        position: LatLng(28.2774, 83.5816),
+        infoWindow: InfoWindow(title: 'ajksd')),
+    Marker(markerId: MarkerId('3'), position: LatLng(28.263610, 83.972390)),
+    Marker(
+        markerId: MarkerId('2'),
+        position: LatLng(28.237988, 83.995590),
+        infoWindow: InfoWindow(title: 'current location'))
   ];
 
   static final CameraPosition _kGoogle = const CameraPosition(
@@ -28,6 +32,16 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.location_searching),
+        onPressed: () async {
+          GoogleMapController controller = await _controller.future;
+          controller.animateCamera(
+            CameraUpdate.newCameraPosition(
+                CameraPosition(target: LatLng(28.263610, 83.972390), zoom: 100)),
+          );
+        },
+      ),
       body: GoogleMap(
         initialCameraPosition: _kGoogle,
         markers: Set<Marker>.of(_marker),
